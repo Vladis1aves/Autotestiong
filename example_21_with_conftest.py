681c5526ac1b89625a0811fbcdb291986b6b786c
vlad <alekseev@spiks.ru>
2023-11-23 18:29:55 +0300

    add example_21

--- example_21_with_conftest.py
+++ example_21_with_conftest.py
@@ -6,26 +6,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-
-
 class TestLink1():
     @pytest.mark.parametrize("link",
                              [
-                                 "https://stepik.org/lesson/236895/step/1"
-                                 # ,
-                                 # "https://stepik.org/lesson/236896/step/1",
-                                 # "https://stepik.org/lesson/236897/step/1",
-                                 # "https://stepik.org/lesson/236898/step/1",
-                                 # "https://stepik.org/lesson/236899/step/1",
-                                 # "https://stepik.org/lesson/236903/step/1",
-                                 # "https://stepik.org/lesson/236904/step/1",
-                                 # "https://stepik.org/lesson/236905/step/1"
+                                 "https://stepik.org/lesson/236895/step/1",
+                                 "https://stepik.org/lesson/236896/step/1",
+                                 "https://stepik.org/lesson/236897/step/1",
+                                 "https://stepik.org/lesson/236898/step/1",
+                                 "https://stepik.org/lesson/236899/step/1",
+                                 "https://stepik.org/lesson/236903/step/1",
+                                 "https://stepik.org/lesson/236904/step/1",
+                                 "https://stepik.org/lesson/236905/step/1"
                              ]
                              )
     def test_login_link_1(self, browser, link):
-        link = "https://stepik.org/lesson/236895/step/1"
         browser.get(link)
-        login = browser.find_element(By.CSS_SELECTOR, "#ember35")
+        login = browser.find_element(By.CSS_SELECTOR, "header nav a[href*='login']")
         login.click()
         email = browser.find_element(By.CSS_SELECTOR, "#id_login_email")
         email.send_keys("email")
@@ -33,17 +29,17 @@ class TestLink1():
         password.send_keys("password")
         button = browser.find_element(By.CSS_SELECTOR, "#login_form > button")
         button.click()
-        # time.sleep(15)
-        # ember96
-        # ember93
-        input_ = browser.find_element(By.CSS_SELECTOR, "#ember93")
-        input_ = WebDriverWait(browser, 5).until(EC.element_located_selection_state_to_be(input_, True))
+        time.sleep(3)
+
+        # again = browser.find_element(By.CSS_SELECTOR, "button[class*='again-btn']")
+        # again.click()
+
+        input_ = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='attempt'] textarea")))
         answer = math.log(int(time.time()))
         input_.send_keys(answer)
-        time.sleep(10)
-        button_send = browser.find_element(By.CSS_SELECTOR, "#ember79 > div > section > div > div.attempt__inner > div.attempt__actions > button")
-        button_send = WebDriverWait(browser, 7).until(EC.element_to_be_clickable(button_send))
+        button_send = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[class*='attempt'] button")))
         button_send.click()
-        time.sleep(30)
-        message = browser.find_element(By.CSS_SELECTOR, "#ember99 > p")
-        assert "Correct!" is message.text
+        message = browser.find_element(By.CSS_SELECTOR, "p[class*='smart-hints']")
+        print(message.text)
+        assert 'Correct!' in message.text, f"Incorrect message = {message.text}"
+
